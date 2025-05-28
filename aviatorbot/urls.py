428273a423de2fcb938add725eb1aa84838ba9d7
andre <andrebeet1@gmail.com>
2025-05-28 20:41:17 +0200

    Update urls.py

--- aviatorbot/urls.py
+++ aviatorbot/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('aviator.urls')),  # Lien vers ton app principale
+    path('', include('aviator.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),  # ⬅️ Ceci est essentiel
 ]
